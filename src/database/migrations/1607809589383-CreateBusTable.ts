1db46184cec122b46ead601639f5ef15e66d0a44
Morramidy <vicoe.sales@gmail.com>
2020-12-12 18:49:16 -0300

    feat(migration): Created Bus migration

---
+++ src/database/migrations/1607809589383-CreateBusTable.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateBusTable1607809589383 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(new Table({
+            name:'bus',
+            columns:[
+            {
+                name:'id',
+                type:'uuid',
+                isPrimary:true,
+                default:'uuid_generate_v4()',
+            },
+            {
+                name:'id_user',
+                type:'int',
+            },
+            {
+                name:'vehicle_plate',
+                type:'varchar',
+                isUnique:true,
+            },
+            {
+                name:'year',
+                type:'int',
+            },
+            {
+                name:'model',
+                type:'varchar',
+            },
+            {
+                name:'seat_amount',
+                type:'varchar',
+            },
+        ],
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('bus');
+    }
+
+}
