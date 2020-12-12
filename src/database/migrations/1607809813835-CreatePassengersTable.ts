13e11b4768b3304af916935a961f8dac29ac15c7
Morramidy <vicoe.sales@gmail.com>
2020-12-12 18:52:25 -0300

    feat(migration): Created Passengers migration

---
+++ src/database/migrations/1607809813835-CreatePassengersTable.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreatePassengersTable1607809813835 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(new Table({
+            name:'passengers',
+            columns:[
+            {
+                name:'id',
+                type:'uuid',
+                isPrimary:true,
+                default:'uuid_generate_v4()',
+            },
+            {
+                name:'name',
+                type:'varchar',
+            },
+            {
+                name:'age',
+                type:'integer'
+            },
+            {
+                name:'cpf',
+                type:'int',
+                isUnique:true,
+            },
+        ],
+        }));
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('passengers');
+
+    }
+
+}
