5425f768b608453523a3dcad66bc20c9fbfde51b
Morramidy <vicoe.sales@gmail.com>
2020-12-12 19:04:10 -0300

    feat(models): Created model Bus

---
+++ src/app/models/Bus.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('bus')
+class Bus{
+
+  @PrimaryGeneratedColumn('uuid')
+  id:string;
+
+  @Column()
+  id_user:number;
+
+  @Column()
+  vehicle_plate:string;
+
+  @Column()
+  year:number;
+
+  @Column()
+  model:string;
+
+  @Column()
+  seat_amount:string;
+}
+
+export default Bus;
