15da84d35eab18ddaaa26b50137aaa4fe18ae7be
Morramidy <vicoe.sales@gmail.com>
2020-12-14 18:35:05 -0300

    feat(function):List all buses / List data from one

--- src/app/Controllers/BusController.ts
+++ src/app/Controllers/BusController.ts
@@ -3,23 +3,61 @@ import { getRepository } from 'typeorm';
 
 import Bus from '../models/Bus';
 
-class BusController{
-  async store(req: Request, res: Response) {
-    const repository = getRepository(Bus);
-    const {id_user, vehicle_plate, year, model,seat_amount} = req.body;
+class  BusController{
 
-    const vehiclePlateEquals = await repository.findOne({where:{vehicle_plate}});
+  async store(req: Request, res: Response) {
+     const repository = getRepository(Bus);
+     const {id_user, vehicle_plate, year, model,seat_amount} = req.body;
+
+     const vehiclePlateEquals = await repository.findOne({where:{vehicle_plate}});
 
     if(vehiclePlateEquals){
       return res.sendStatus(409);
     }
 
-    const bus =  repository.create({id_user, vehicle_plate, year, model, seat_amount});
-    await repository.save(bus);
+     const bus =  repository.create({id_user, vehicle_plate, year, model, seat_amount});
+     await repository.save(bus);
 
     return res.json(bus);
   }
 
+
+   async list(req: Request, res:Response){
+      const repository = getRepository(Bus);
+  
+      const bus = await repository.find();
+      const result = Object.entries(bus).length;
+
+      if(result == 0){
+        return res.status(404).send(bus);
+      }
+
+      return res.status(200).json(bus);
+  }
+
+   async specificListing(req: Request, res: Response){
+      const repository = getRepository(Bus);
+      const {id} = req.params;
+
+      try {
+
+      const bus =  await repository.findOneOrFail({id:id});
+      console.log(bus);
+      const result = Object.entries(bus).length;
+
+      if(result == 0){
+        return res.status(404).send(bus);
+      }
+      return res.status(200).json(bus);
+        
+      } catch{
+        
+        return res.sendStatus(400);
+      } 
+  }
+
+  
 }
+
 
 export default new BusController;
--- src/router.ts
+++ src/router.ts
@@ -5,12 +5,17 @@ import { multerConfig } from './app/config/multer';
 import UserController from './app/Controllers/UserController';
 import BusController from './app/Controllers/BusController';
 import Passengers from './app/Controllers/Passengers';
+import TravelController from './app/Controllers/TravelsController';
 
 const router = Router();
+
+router .get('/bus',BusController.list);
+router .get('/bus/:id',BusController.specificListing);
 
 router.post('/users',multer(multerConfig).single('image'),UserController.store);
 router .post('/bus',BusController.store);
 router.post('/passenger',Passengers.store);
+router.post('/travel',TravelController.store);
 
 export default router;
 
