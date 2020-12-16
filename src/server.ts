89fc9832d64ec6be7b114780b3f1b8395c0ebbf4
Morramidy <vicoe.sales@gmail.com>
2020-12-16 19:42:12 -0300

    fix(Port):Correction of the execution port

--- src/server.ts
+++ src/server.ts
@@ -11,4 +11,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333,()=>console.log("🔥 Server Started ✅"));
+app.listen(process.env.PORT || 3333,()=>console.log("🔥 Server Started ✅"));
